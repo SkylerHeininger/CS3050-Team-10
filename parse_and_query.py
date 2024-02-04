2dce9025e9ee4e4dad03ed3b4c8b01511fd5c701
skylerheininger <skyler.heininger@uvm.edu>
2024-02-04 14:08:37 -0500

    creation of main program

---
+++ parse_and_query.py
@@ -0,0 +1,32 @@
+"""
+CS3010 team 10
+Warmup project
+This contains the engines for the warmup project, and many other functions.
+"""
+
+
+def parse(input_string):
+    """
+
+    :param input_string:
+    :return:
+    """
+
+
+def query_firestore(conditional):
+    """
+
+    :param conditional:
+    :return:
+    """
+
+
+def query_engine(conditionals):
+    """
+    This function is the engine that will query the database.
+    This will handle multiple conditionals and create a list of objects
+    :param conditionals:
+    :return:
+    """
+
+
